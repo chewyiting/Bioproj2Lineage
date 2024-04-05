8d562d775e1366104ddf12ca013e93be69de0bc0
Yi-Ting <65509063+chewyiting@users.noreply.github.com>
2024-04-05 14:54:01 -0400

    Update slurm_Accs2Runaccs.sh

--- slurm_Accs2Runaccs.sh
+++ slurm_Accs2Runaccs.sh
@@ -7,37 +7,32 @@
 #SBATCH --time=0:10:00
 #SBATCH --output=Accs2Runaccs_%A.out
 #SBATCH --error=Accs2Runaccs_%A.err
-#SBATCH --account=cohen_theodore
+#SBATCH --account=XXXXXXXXXXXXXXXX
 
 ###########################
-# options
+# options (!!modify before runnning!!)
 ###########################
-dirtemplate='/home/yc954/project.cohen/sm_Bioproj2Lineage/dirTemplate/'
+dirtemplate='/path/to/cloned/repo/Bioproj2Lineage/' # this should point to the cloned github repo! 
 bioproj='PRJNA736718'
 batchno=23
 
 # config parameters for Snakefile (sm_Accs2Runaccs)
-sras=$(echo '/home/yc954/project.cohen/sm_Bioproj2Lineage/dirTemplate/batchedtsvs/'${bioproj}'_batch'${batchno}'_sras.tsv')
-reference='/home/yc954/project.cohen/sm_Bioproj2Lineage/dirTemplate/reference/NC_000962_3.fa'
+sras=$(echo ${dirtemplate}'batchedtsvs/'${bioproj}'_batch'${batchno}'_sras.tsv')
+reference=$(echo ${dirtemplate}'reference/NC_000962_3.fa')
 wanted_lineage='lineage4.2'
 basedir=$(pwd)
-dirscript='/home/yc954/project.cohen/sm_Bioproj2Lineage/dirTemplate/scripts/'
-
-# other parameters
-myinputdir='sample_info'
-myoutputdir='json_summarise'
-myoutputname='js_summarise_dummy.tsv'
+dirscript=$(echo ${dirtemplate}'scripts/')
 
 ###########################
-# script
+# script 
 ###########################
-# 1. prepare sm_Accs2Runaccs
+# prepare sm_Accs2Runaccs
 mkdir -p sm_Accs2Runaccs
-cp -n ${dirtemplate}sm_Accs2Runaccs/Snakefile ./sm_Accs2Runaccs
+cd ./sm_Accs2Runaccs
+cp -n ${dirtemplate}sm_Accs2Runaccs/Snakefile .
 
 module purge
 module load miniconda/23.5.2
 conda activate snakemake
 conda activate bioinfo
-cd ./sm_Accs2Runaccs
 snakemake --cores all --config sras=${sras} reference=${reference} wanted_lineage=${wanted_lineage} basedir=${basedir}/sm_Accs2Runaccs/ dirscript=${dirscript}
